b6c4b1aa53e9b7c382eff3a53a993e9049870b13
KHALID-ZENNOUHI <khalidzennouhi08@gmail.com>
2024-04-25 21:42:43 +0100

    Refactor teacher_dashboard.blade.php to update dashboard icons and values

--- resources/views/dashboard/teacher_dashboard.blade.php
+++ resources/views/dashboard/teacher_dashboard.blade.php
@@ -26,11 +26,11 @@
                             <div class="db-widgets d-flex justify-content-between align-items-center">
                                 <div class="db-info">
                                     <h6>Week Classes</h6>
-                                    <h3></h3>
+                                    <h3>0</h3>
                                 </div>
-                                <div class="db-icon">
+                                {{-- <div class="db-icon">
                                     <img src="{{ URL::to('assets/img/icons/teacher-icon-01.svg') }}" alt="Dashboard Icon">
-                                </div>
+                                </div> --}}
                             </div>
                         </div>
                     </div>
@@ -41,11 +41,11 @@
                             <div class="db-widgets d-flex justify-content-between align-items-center">
                                 <div class="db-info">
                                     <h6>Total Students</h6>
-                                    <h3>40/60</h3>
+                                    <h3>0</h3>
                                 </div>
-                                <div class="db-icon">
+                                {{-- <div class="db-icon">
                                     <img src="{{ URL::to('assets/img/icons/dash-icon-01.svg') }}" alt="Dashboard Icon">
-                                </div>
+                                </div> --}}
                             </div>
                         </div>
                     </div>
@@ -58,9 +58,9 @@
                                     <h6>Week Lessons</h6>
                                     <h3>{{$cours->count()}}</h3>
                                 </div>
-                                <div class="db-icon">
+                                {{-- <div class="db-icon">
                                     <img src="{{ URL::to('assets/img/icons/teacher-icon-02.svg') }}" alt="Dashboard Icon">
-                                </div>
+                                </div> --}}
                             </div>
                         </div>
                     </div>
@@ -71,11 +71,11 @@
                             <div class="db-widgets d-flex justify-content-between align-items-center">
                                 <div class="db-info">
                                     <h6>Total Hours</h6>
-                                    <h3>15/20</h3>
+                                    <h3>0</h3>
                                 </div>
-                                <div class="db-icon">
+                                {{-- <div class="db-icon">
                                     <img src="{{ URL::to('assets/img/icons/teacher-icon-03.svg') }}" alt="Dashboard Icon">
-                                </div>
+                                </div> --}}
                             </div>
                         </div>
                     </div>
@@ -83,7 +83,7 @@
             </div>
 
 
-            <div class="row">
+            {{-- <div class="row">
                 <div class="col-12 col-lg-12 col-xl-8">
                     <div class="row">
                         <div class="col-12 col-lg-8 col-xl-8 d-flex">
@@ -368,7 +368,7 @@
                         </div>
                     </div>
                 </div>
-            </div>
+            </div> --}}
         </div>
     </div>
 @endsection
