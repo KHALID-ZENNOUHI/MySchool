8c98cfebbf757edbc47eea3e2aa3be2fafe87202
KHALID-ZENNOUHI <khalidzennouhi08@gmail.com>
2024-04-25 10:56:45 +0100

    Update AnneScolaireSeeder.php to include 'nom' field in $fillable array

--- database/seeders/AnneScolaireSeeder.php
+++ database/seeders/AnneScolaireSeeder.php
@@ -13,16 +13,13 @@ class AnneScolaireSeeder extends Seeder
     public function run(): void
     {
         $anneeScolaire = [
-            ['annee_scolaire_start' => '2021-09-01', 'annee_scolaire_end' => '2022-06-30'],
-            ['annee_scolaire_start' => '2022-09-01', 'annee_scolaire_end' => '2023-06-30'],
-            ['annee_scolaire_start' => '2023-09-01', 'annee_scolaire_end' => '2024-06-30'],
-            ['annee_scolaire_start' => '2024-09-01', 'annee_scolaire_end' => '2025-06-30'],
-            ['annee_scolaire_start' => '2025-09-01', 'annee_scolaire_end' => '2026-06-30'],
-            ['annee_scolaire_start' => '2026-09-01', 'annee_scolaire_end' => '2027-06-30'],
-            ['annee_scolaire_start' => '2027-09-01', 'annee_scolaire_end' => '2028-06-30'],
-            ['annee_scolaire_start' => '2028-09-01', 'annee_scolaire_end' => '2029-06-30'],
-            ['annee_scolaire_start' => '2029-09-01', 'annee_scolaire_end' => '2030-06-30'],
-            ['annee_scolaire_start' => '2030-09-01', 'annee_scolaire_end' => '2031-06-30'],
+            ['nom' => 2024],
+            ['nom' => 2025],
+            ['nom' => 2026],
+            ['nom' => 2027],
+            ['nom' => 2028],
+            ['nom' => 2029],
+            ['nom' => 2030],
         ];
 
         foreach ($anneeScolaire as $annee) {
