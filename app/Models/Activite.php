6ac294cf6ece08bba4f486d4e66bad69117b4a10
KHALID-ZENNOUHI <khalidzennouhi08@gmail.com>
2024-05-18 17:07:05 +0100

    Add 'matiere_id' relationship to Activite model

--- app/Models/Activite.php
+++ app/Models/Activite.php
@@ -9,10 +9,15 @@ class Activite extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['type', 'date', 'ressources','title', 'description', 'classe_id', 'formateur_id'];
+    protected $fillable = ['type', 'date', 'ressources','title', 'description', 'classe_id', 'formateur_id', 'matiere_id'];
 
     public function classe()
     {
         return $this->belongsTo(Classe::class);
+    }
+
+    public function matiere()
+    {
+        return $this->belongsTo(Matiere::class);
     }
 }
